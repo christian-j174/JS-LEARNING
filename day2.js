6f185a3faa1dbeca0eada0e46dad250fa7a01e43
christian-j174 <christian.rodriguez95@upr.edu>
2022-09-26 22:23:09 -0400

    commit1

---
+++ day2.js
@@ -0,0 +1,24 @@
+
+//EXAMPLE OF HOISTING
+// fun();
+// function fun() {
+//     console.log('Hi')
+// }
+
+//CLOSURES - definir funciones dentro de otras
+
+// function outer() {
+//     let count = 0;
+
+//     function inner() {
+//         count ++;
+//         return count;
+//     }
+
+//     return inner;
+// }
+
+
+// const addOne = outer();
+
+// console.log(addOne())
---
+++ functions.js
@@ -0,0 +1,15 @@
+// function makeBreakfast(main, side, drink) {
+//     console.log(arguments);
+//     return 'Breakfast is ${main}, ${side}, ${drink}.';
+// }
+
+
+// function makeLunch (opts) {
+//     const { main, side, drink } = opts;
+//     return `Lunch is ${main}, ${side}, and ${drink}.`;
+// }
+
+// makeBreakfast('🥞', '🥓', '🥛');
+// //makeLunch({ main: '🥙', side: '🍟', drink: '🥤' });
+
+
